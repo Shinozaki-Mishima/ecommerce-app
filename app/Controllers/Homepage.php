e7314883b18d8d9b54652438f4f971b94a43eb15
Shinozaki-Mishima <shivandavi56@gmail.com>
2022-09-13 12:38:47 -0400

    updated required files in homepage.

--- app/Controllers/Homepage.php
+++ app/Controllers/Homepage.php
@@ -1,8 +1,13 @@
 <?php
 
-require_once APP_DIR."Utils/code.precheckout.php";
+require_once APP_DIR."Config/Database.php";
+require_once APP_DIR."Models/User.php";
+require_once APP_DIR."Models/Product.php";
 
 
+$db_object = new Database();
+$user_object = new User($db_object);
+$product_object = new Product($db_object);
 
 // required views
 require_once APP_DIR."Views/header-1.php";
